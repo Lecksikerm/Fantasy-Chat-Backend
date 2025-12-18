168b007d8fd6cbbb85d2b39380bb0e4c3f3e5a5e
kareem idris <idrisolalekann@gmail.com>
2025-12-18 15:13:54 +0100

    Add project structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import app from "./app";
+import { connectDB } from "./config/db";
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+  await connectDB();
+  app.listen(PORT, () =>
+    console.log(`Server listening on port ${PORT}`)
+  );
+})();
