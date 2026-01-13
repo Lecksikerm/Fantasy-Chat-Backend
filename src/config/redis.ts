3250a6e60b96d1619c7437e29b950bbaa9b1c522
kareem idris <idrisolalekann@gmail.com>
2026-01-13 10:48:32 +0100

    Render for deployment

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,14 +1,29 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
+let redis: Redis;
+
+if (process.env.REDIS_URL) {
+  
+  redis = new Redis(process.env.REDIS_URL, {
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+
+  redis = new Redis({
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: Number(process.env.REDIS_PORT) || 6379
-});
+  });
+}
 
 redis.on("connect", () => {
-    console.log("Redis connected");
+  console.log("Redis connected");
 });
 
 redis.on("error", (err) => {
-    console.error("Redis error", err);
+  console.error("Redis error", err);
 });
+
+export { redis };
+
